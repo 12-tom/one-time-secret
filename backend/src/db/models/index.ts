3188902e03ebf4b326a8c6e7b9fb4dfabc487bc8
Thomas Justine <thomasjustine.mvraaa@gmail.com>
2024-10-03 10:10:54 +0530

    feat: resolved sequelize model comments

--- backend/src/db/models/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import pg from "pg";
-
-dotenv.config();
-
-const conString: string = process.env.DATABASE_URL as string;
-const client = new pg.Client(conString);
-
-export default client;
